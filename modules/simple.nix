c7b5090bd7391e10a09514c7a442b2f3109d89b8
Janne Heß <janne@hess.ooo>
2020-06-21 17:10:39 +0200

    modules: Add the simple module

---
+++ modules/simple.nix
@@ -0,0 +1,28 @@
+{ config, lib, ... }: with lib; let
+  cfg = config.services.bigbluebutton.simple;
+in {
+  options.services.bigbluebutton.simple = with types; {
+    enable = mkEnableOption "a simple one-node BigBlueButton installation";
+
+    domain = mkOption {
+      description = "Domain under which to serve this BigBlueButton";
+      type = str;
+      example = "bbb.example.com";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.bigbluebutton = {
+      akka-fsesl.enable = true;
+      akka-apps = {
+        enable = true;
+        config.services.bbbWebAPI = "https://${cfg.domain}/bigbluebutton/api";
+      };
+      web = {
+        enable = true;
+        config."bigbluebutton.web.serverURL" = "https://${cfg.domain}";
+      };
+      kurento-media-server.enable = true;
+    };
+  };
+}
