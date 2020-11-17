1a7648f7e6336fac602479d47a2aa94e09e43814
ajs124 <git@ajs124.de>
2020-11-17 03:45:12 +0100

    packages/bigbluebutton: v2.2.23 -> v2.2.29
    some patches needed updating/importing because they did not apply anymore

--- packages/sources/bigbluebutton/raw-source.nix
+++ packages/sources/bigbluebutton/raw-source.nix
@@ -1,6 +1,6 @@
 { fetchFromGitHub }: fetchFromGitHub {
   owner = "bigbluebutton";
   repo = "bigbluebutton";
-  rev = "v2.2.23";
-  sha256 = "sha256-HPm5pDDi6RxxcwYhr2XJdmNKSuF0n6sKL4Ewdnh5NSU=";
+  rev = "v2.2.29";
+  sha256 = "sha256-BjjGxWWyRnF3th+dc4WV1dyDSX7kyAzdFuANWJiCXkM=";
 }
