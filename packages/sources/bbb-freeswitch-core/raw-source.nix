50cec2ad42a74fdff18512265d355ad9d3b65864
ajs124 <git@ajs124.de>
2020-11-17 03:42:43 +0100

    packages/bbb-freeswitch-core: 2.2.0-112 -> 2.2.0-113

--- packages/sources/bbb-freeswitch-core/raw-source.nix
+++ packages/sources/bbb-freeswitch-core/raw-source.nix
@@ -1,6 +1,6 @@
 { fetchurl }:
 
 fetchurl {
-  url = "https://ubuntu.bigbluebutton.org/xenial-22/pool/main/b/bbb-freeswitch-core/bbb-freeswitch-core_2.2.0-112_amd64.deb";
-  sha256 = "ba4eb3937209429be32c180f32bcee09c4782d5028ceb5fa46badcd3ede59e91";
+  url = "https://ubuntu.bigbluebutton.org/xenial-22/pool/main/b/bbb-freeswitch-core/bbb-freeswitch-core_2.2.0-113_amd64.deb";
+  sha256 = "dcbe172309ef4e55695fe36a7ed622d7781624cf7c30bc58f4b0e81b093f021f";
 }
