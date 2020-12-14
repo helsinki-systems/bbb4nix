bb38169487c2ddd1c9a9cbb27a0f8d2b206ad2dc
ajs124 <git@ajs124.de>
2020-12-14 05:00:36 +0100

    packages/bbb-greenlight: 2.7.9 -> 2.7.11

--- packages/sources/bbb-greenlight/raw-source.nix
+++ packages/sources/bbb-greenlight/raw-source.nix
@@ -1,6 +1,6 @@
 { fetchFromGitHub }: fetchFromGitHub {
   owner = "bigbluebutton";
   repo = "greenlight";
-  rev = "release-2.7.9";
-  sha256 = "sha256-w4fiDsZnNwWCBSDtviqqi2OFR1ijR5K97qzdj4ReE9A=";
+  rev = "release-2.7.11";
+  sha256 = "sha256-icUZ/Ue8p/jF6JyN92qhvv5/+Mw8wpY1lincXu+pAyM=";
 }
