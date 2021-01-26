a5bf7a7dfcdcc3720c6153b90aa4a0ba34b1ebf0
ajs124 <git@ajs124.de>
2021-01-26 20:17:55 +0100

    packages/bbb-webrtc-sfu: nodejs-12 -> nodejs

--- packages/bbb-webrtc-sfu/default.nix
+++ packages/bbb-webrtc-sfu/default.nix
@@ -1,27 +1,26 @@
-{ stdenv, callPackage, makeWrapper, nodePackages_12_x
-, python2, util-linux, runCommand, writeTextFile, nodejs-12_x, darwin
+{ stdenv, callPackage, makeWrapper, nodePackages
+, python2, util-linux, runCommand, writeTextFile, nodejs, darwin
 , fetchurl, fetchgit }:
 let
   src = callPackage ../sources/bbb-webrtc-sfu {};
 
   nodeEnv = import ../x2nix/node-env.nix {
-    inherit stdenv python2 util-linux runCommand writeTextFile;
-    nodejs = nodejs-12_x;
+    inherit stdenv nodejs python2 util-linux runCommand writeTextFile;
     libtool = if stdenv.isDarwin then darwin.cctools else null;
   };
 
-  nodePackages = import ./node-packages.nix {
+  _nodePackages = import ./node-packages.nix {
     inherit nodeEnv fetchurl fetchgit;
-    globalBuildInputs = [ nodePackages_12_x.node-gyp-build ];
+    globalBuildInputs = with nodePackages; [ node-gyp-build ];
   };
-in nodeEnv.buildNodePackage (nodePackages.args // {
+in nodeEnv.buildNodePackage (_nodePackages.args // {
   inherit src;
 
   nativeBuildInputs = [ makeWrapper ];
   postInstall = ''
     mkdir -p $out/bin
 
-    makeWrapper ${nodejs-12_x}/bin/node $out/bin/bbb-webrtc-sfu \
+    makeWrapper ${nodejs}/bin/node $out/bin/bbb-webrtc-sfu \
       --run 'cd ${placeholder "out"}/lib/node_modules/bbb-webrtc-sfu' \
       --add-flags ./server.js
   '';
