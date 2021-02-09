c66c5e2f6eed15350450bd95a9f1c9beca55f8a8
ajs124 <git@ajs124.de>
2021-02-09 03:01:38 +0100

    packages/bbb: 2.2.30 -> 2.2.31
    (cherry picked from commit 60330f3f8de0fabdaabe25763d61e781df73e0ac)

--- packages/sources/bbb-etherpad-lite/raw-source.nix
+++ packages/sources/bbb-etherpad-lite/raw-source.nix
@@ -1,6 +1,6 @@
 { fetchFromGitHub }: fetchFromGitHub {
   owner = "bigbluebutton";
   repo = "etherpad-lite";
-  rev = "a0e818cc0dd8c0a495fae6cbf5976744236daab8";
-  sha256 = "sha256-RtxFNqLX7BNkKhD6dtzLIPCcQbceY88tgUq4XSbNYvE=";
+  rev = "2dbc25b90a5ea164458762154c7954cd04dadffc";
+  sha256 = "sha256:021majhics60xdssrq4i16wyxan9wfa4bs0sa3nmm9cl9r3199mi";
 }
--- packages/sources/bigbluebutton/raw-source.nix
+++ packages/sources/bigbluebutton/raw-source.nix
@@ -1,6 +1,6 @@
 { fetchFromGitHub }: fetchFromGitHub {
   owner = "bigbluebutton";
   repo = "bigbluebutton";
-  rev = "v2.2.30";
-  sha256 = "sha256-FthPgYkrRYzdYiRrTHtcsQCvlUqH7aoyYvtb7lxT89Q=";
+  rev = "v2.2.31";
+  sha256 = "sha256:11hqn5441a6jwvy1qk6wdlypmi120kk8rq4zg5ybmk34mwdlzlwi";
 }
