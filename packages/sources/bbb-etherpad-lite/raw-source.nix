f9984f1d990c443a537da0fefc50bb407c6188ad
ajs124 <git@ajs124.de>
2021-04-05 03:24:49 +0200

    packages/bbb-etherpad-lite: update
    (cherry picked from commit 9085ec7d39254209bf81f0387843185f24b1c235)

--- packages/sources/bbb-etherpad-lite/raw-source.nix
+++ packages/sources/bbb-etherpad-lite/raw-source.nix
@@ -1,6 +1,6 @@
 { fetchFromGitHub }: fetchFromGitHub {
   owner = "bigbluebutton";
   repo = "etherpad-lite";
-  rev = "2dbc25b90a5ea164458762154c7954cd04dadffc";
-  sha256 = "sha256:021majhics60xdssrq4i16wyxan9wfa4bs0sa3nmm9cl9r3199mi";
+  rev = "50fd563ab4312482a7c764a30be261d9c9dc02ba";
+  sha256 = "sha256:0vjj47nxvk0i387kiqkha0r2wz9jyi1fl0gglhsmpbkadyzf2nsw";
 }
