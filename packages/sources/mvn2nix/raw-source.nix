e589360ba19f14caf17cc60075201084ca9b7655
ajs124 <git@ajs124.de>
2021-07-08 14:28:41 +0200

    packages/mvn2nix: fix hash
    (cherry picked from commit 21340954e43f469d8f6047831c993be2ccd6c75f)

--- packages/sources/mvn2nix/raw-source.nix
+++ packages/sources/mvn2nix/raw-source.nix
@@ -2,6 +2,6 @@
   owner = "fzakaria";
   repo = "mvn2nix";
   rev = "3af3a7e02bce9cccc77c582a589d742e4fb8540e";
-  sha256 = "sha256:0a07cw6n7lkp9j7xfl4p7n52m3mil98mclgy3dk89khj8sj7h3mi";
+  sha256 = "1gq4sc0cwlhkka89nzshb2gvk9k9jbf3slrz56lbhgcv3xhk1x6c";
   leaveDotGit = true;
 }
