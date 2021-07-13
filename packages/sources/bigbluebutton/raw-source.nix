54b578145c994180583db5602a988c0afbf382c1
ajs124 <git@ajs124.de>
2021-07-14 01:15:13 +0200

    packages/bbb: 2.2.36 -> 2.3.6

--- packages/sources/bigbluebutton/raw-source.nix
+++ packages/sources/bigbluebutton/raw-source.nix
@@ -1,6 +1,6 @@
 { fetchFromGitHub }: fetchFromGitHub {
   owner = "bigbluebutton";
   repo = "bigbluebutton";
-  rev = "v2.2.36";
-  sha256 = "sha256:1y8bdrh82n69cjc3qc2zy41wyirkkswgjsa9gn0pylbkgr43iw24";
+  rev = "v2.3.6";
+  sha256 = "sha256:1ssw6p58iswallndi9bnxv30hp1hn4yl7jj89zp053g0lkp8yz8m";
 }
