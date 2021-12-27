556269053ada70e9140928ceb48db2651733faa4
ajs124 <git@ajs124.de>
2021-12-27 17:17:55 +0100

    packages/bbb-webrtc-sfu: 2.5.2 -> 2.5.10

--- packages/sources/bbb-webrtc-sfu/raw-source.nix
+++ packages/sources/bbb-webrtc-sfu/raw-source.nix
@@ -1,6 +1,6 @@
 { fetchFromGitHub }: fetchFromGitHub {
   owner = "bigbluebutton";
   repo = "bbb-webrtc-sfu";
-  rev = "v2.5.2";
-  sha256 = "sha256:08hajnypqr1lf68mwijyn8hnvfic0xylmg15b2q979xcsryrjj16";
+  rev = "v2.5.10";
+  sha256 = "sha256:00m5i2gw0xhnhycvlyvwkrn4bj99smhm2ykam61z11y0m09rvyxa";
 }
