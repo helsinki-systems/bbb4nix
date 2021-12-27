0a1d13685da23bf45e94b90213ab0c9a926c34bf
ajs124 <git@ajs124.de>
2021-12-27 16:22:15 +0100

    packages/greenlight: 2.9.0 -> 2.10.0.3

--- packages/sources/bbb-greenlight/raw-source.nix
+++ packages/sources/bbb-greenlight/raw-source.nix
@@ -1,6 +1,6 @@
 { fetchFromGitHub }: fetchFromGitHub {
   owner = "bigbluebutton";
   repo = "greenlight";
-  rev = "release-2.9.0";
-  sha256 = "sha256:16nixws14v1h2mprgfxyzwnvigvjgjahgfiy0b0in6jcx382ixns";
+  rev = "release-2.10.0.3";
+  sha256 = "sha256:1vlyvjrdvx73x90sx66vcvkfs6mkgc586z0ds5ar9l58zz5m7wwj";
 }
